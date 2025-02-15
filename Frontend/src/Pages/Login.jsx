d112f2d5d5e35f9ec155ad2e99a34716d0f2c007
Colin <103554470+jwtic@users.noreply.github.com>
2025-02-15 14:49:34 +0800

    login update

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -10,54 +10,68 @@ const Login = () => {
 
       
       <div className="login-page">
-
             <motion.div
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 2, y: 0 }}
         transition={{ duration:0.5 }}>
-       
-        
+  
         <NavBar />
-        <div className="content-wrapper" data-aos="zoom-in">
 
-          <div className="login-container">
-            <h1 className = "LegaLens">LegaLens.</h1>
-            <form>
-              <div className="form-group">
-                <label className="user-name">Username</label>
-                <input
-                  className = "userinput"
-                  type="text"
-                  id="userinput"
-                  placeholder="Enter your username"
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label className="password">
-                  Password{' '}
-                  <span className="toggle-password">
-                    Show
-                  </span>
-                </label>
-                <input
-                  className = "passwordinput"
-                  type="password"
-                  id="passwordinput"
-                  placeholder="Enter your password"
-                  required
-                />
-              </div>
-              <div className="auth-links">
-                <Link to="/register" className="register-link">
-                  Register an Account
-                </Link>
-                <Link to="/forgot-password">Forgot your password?</Link>
-              </div>
-              <button type="submit">Login</button>
-            </form>
-          </div>
+        <div className="content-wrapper" data-aos="zoom-in">
+  <div className="container">
+    {/* Header Row */}
+      <div className="col-12">
+        <h1 className="LegaLens">LegaLens.</h1>
+      </div>
+  
+
+    <div className="row">
+      <div className="col-12">
+        <div className="login-container">
+          <form>
+            <div className="form-group">
+              <label className="user-name">Username</label>
+              <input
+                className="form-control text-left userinput"
+                type="text"
+                id="userinput"
+                placeholder="Enter your username"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label className="password">
+                Password{' '}
+                <span className="toggle-password">
+                  Show
+                </span>
+              </label>
+              <input
+                className="form-control text-left passwordinput"
+                type="password"
+                id="passwordinput"
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+            <div className="auth-links">
+              <Link to="/register" className="register-link">
+                Register an Account
+              </Link>
+              <Link to="/forgot-password">Forgot your password?</Link>
+            </div>
+            <button type="submit">Login</button>
+            <div>Or</div>
+
+            
+
+          </form>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+
         
       </motion.div>
       </div>
