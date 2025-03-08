0846a56e90ffc7785ca601fe9e28991eac7ae584
wenkai <xwkofficial@gmail.com>
2025-03-08 17:27:29 +0800

    amend login stuff

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from './components/routeProtection/ProtectedRoute.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 // Public Access Screens
 import Navbar from "./Pages/Navbar.jsx";
@@ -16,7 +14,7 @@ import Home from "./Pages/Home";
 import UploadContract from "./Pages/Upload.jsx";
 import Dashboard  from './Pages/Dashboard.jsx'; 
 import AnalysisPage from './Pages/Analysis.jsx'; 
-import History from './Pages/History.jsx'
+import History from './Pages/History.jsx';
 import ContractCompare from './Pages/CompareContracts.jsx';
 import ContractComparison from './Pages/ContractComparison.jsx';
 
@@ -26,34 +24,21 @@ function App() {
   return (
     <Router>
       <Navbar />
-
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="upload" element={<UploadContract />} />
-                <Route path="home" element={<Dashboard />} />
-                <Route path="analysis" element={<AnalysisPage />} />
-                <Route path="history" element={<History />} />
-                <Route path="compare" element={<ContractCompare />} />
-                <Route path="contractcompare" element={<ContractComparison />} />
+        <Route path="/upload" element={<UploadContract />} />
+        <Route path="/home" element={<Dashboard />} />
+        <Route path="/analysis" element={<AnalysisPage />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/compare" element={<ContractCompare />} />
+        <Route path="/contractcompare" element={<ContractComparison />} />
 
         {/* Protected Routes for Authenticated Users */}
-        <Route
-          path="/user/*"
-          element={
-            <ProtectedRoute>
-              <Routes>
-                <Route path="upload" element={<UploadContract />} />
-                <Route path="home" element={<Dashboard />} />
-                <Route path="analysis" element={<AnalysisPage />} />
-                <Route path="history" element={<History />} />
-                <Route path="compare" element={<ContractCompare />} />
-                <Route path="contractcompare" element={<ContractComparison />} />
-              </Routes>
-            </ProtectedRoute>
-          }
-        />
+        <Route element={<ProtectedRoute />}>
+        {/* Place protected routes here */}
+        </Route>
       </Routes>
     </Router>
   );
