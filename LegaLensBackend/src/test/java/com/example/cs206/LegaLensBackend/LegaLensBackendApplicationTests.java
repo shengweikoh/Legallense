f505cc53c7d0109e847e5faeda9a109ab6773b74
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2025-03-10 10:26:36 +0800

    starter code

---
+++ LegaLensBackend/src/test/java/com/example/cs206/LegaLensBackend/LegaLensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs206.LegaLensBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegaLensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
