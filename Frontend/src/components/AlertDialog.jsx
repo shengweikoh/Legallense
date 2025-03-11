e831cdfeaa51c6839c75551ec0071c2acc0c5ffa
weiqichua <143248754+weiqichua@users.noreply.github.com>
2025-03-11 14:11:58 +0800

    update colour of buttons

--- Frontend/src/components/AlertDialog.jsx
+++ Frontend/src/components/AlertDialog.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, createTheme, ThemeProvider } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,43 +7,60 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import PropTypes from "prop-types";
 
-
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#000000', // black primary color
+        contrastText: '#ffffff',
+      },
+      secondary: {
+        main: '#ffffff', // white secondary color
+        contrastText: '#000000',
+      },
+      background: {
+        default: '#ffffff',
+        paper: '#f7f7f7'
+      },
+    },
+  });
 
 function AlertDialog({open, handleOpen, handleClose, dialogButtonText, dialogTitle, dialogContent, handleAction, actionButtonText}) {
 
     return (
-        <Box>
-            <Button variant="contained" onClick={handleOpen} sx = {{ textTransform: 'capitalize'}}>
-                {dialogButtonText}
-            </Button>
+        <ThemeProvider theme = {theme}>
+            <Box>
+                <Button variant="contained" onClick={handleOpen} sx = {{ textTransform: 'capitalize'}}>
+                    {dialogButtonText}
+                </Button>
 
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby='dialog-title'
-                aria-describedby='dialog-content'
-            > 
-                <DialogTitle id = 'dialog-title' sx = {{fontWeight: 'bold'}}>
-                    {dialogTitle}
-                </DialogTitle >
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby='dialog-title'
+                    aria-describedby='dialog-content'
+                > 
+                    <DialogTitle id = 'dialog-title' sx = {{fontWeight: 'bold'}}>
+                        {dialogTitle}
+                    </DialogTitle >
 
-                <DialogContent>
-                    <DialogContentText id = 'dialog-content'>
-                        {dialogContent}
-                    </DialogContentText>
-                </DialogContent>
+                    <DialogContent>
+                        <DialogContentText id = 'dialog-content'>
+                            {dialogContent}
+                        </DialogContentText>
+                    </DialogContent>
 
-                <DialogActions>
-                    <Button variant='contained' onClick={handleAction} sx={{ textTransform: 'capitalize'}}>
-                        {actionButtonText}
-                    </Button>
-                    <Button variant='outlined' onClick = {handleClose} sx={{ textTransform: 'capitalize'}}>
-                        Cancel
-                    </Button>
+                    <DialogActions>
+                        <Button variant='contained' onClick={handleAction} sx={{ textTransform: 'capitalize'}}>
+                            {actionButtonText}
+                        </Button>
+                        <Button variant='outlined' onClick = {handleClose} sx={{ textTransform: 'capitalize'}}>
+                            Cancel
+                        </Button>
 
-                </DialogActions>
-            </Dialog>
-        </Box>
+                    </DialogActions>
+                </Dialog>
+            </Box>
+        </ThemeProvider>
     );
 }
 
