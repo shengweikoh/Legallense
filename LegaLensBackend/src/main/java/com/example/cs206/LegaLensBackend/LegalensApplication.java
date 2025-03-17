468d4fd5a46129e530792e3110d6fc80cc5a2b2b
weiqichua <143248754+weiqichua@users.noreply.github.com>
2025-03-17 21:47:55 +0800

    rename application

--- LegaLensBackend/src/main/java/com/example/cs206/LegaLensBackend/LegalensApplication.java
+++ LegaLensBackend/src/main/java/com/example/cs206/LegaLensBackend/LegalensApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeminiApplication {
+public class LegalensApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GeminiApplication.class, args);
+        SpringApplication.run(LegalensApplication.class, args);
     }
 }
