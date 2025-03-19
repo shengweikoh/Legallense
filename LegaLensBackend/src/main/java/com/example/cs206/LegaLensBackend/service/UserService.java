cbe590fbb3b355e2333828d2c21ce62f69e07b47
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2025-03-19 14:26:18 +0800

    Fixed some stuff

--- LegaLensBackend/src/main/java/com/example/cs206/LegaLensBackend/service/UserService.java
+++ LegaLensBackend/src/main/java/com/example/cs206/LegaLensBackend/service/UserService.java
@@ -1,31 +1,31 @@
-// package com.example.cs206.LegaLensBackend.service;
+package com.example.cs206.LegaLensBackend.service;
 
-// import com.example.cs206.LegaLensBackend.model.User;
-// import com.google.cloud.firestore.DocumentSnapshot;
-// import com.google.cloud.firestore.Firestore;
+import com.example.cs206.LegaLensBackend.model.User;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// import org.springframework.stereotype.Service;
-// @Service
-// public class UserService {
+import org.springframework.stereotype.Service;
+@Service
+public class UserService {
 
-//     private final Firestore firestore;
+    private final Firestore firestore;
 
-//     public UserService(Firestore firestore) {
-//         this.firestore = firestore;
-//     }
+    public UserService(Firestore firestore) {
+        this.firestore = firestore;
+    }
 
-//     public Optional<User> getUserById(String userId) throws Exception {
-//         DocumentSnapshot snapshot = firestore.collection("Users").document(userId).get().get();
-//         if (snapshot.exists()) {
-//             User user = snapshot.toObject(User.class);
-//             // System.out.println("Raw data: " + snapshot.getData());
-//             // System.out.println("Fetched user: " + user);
-//             return Optional.ofNullable(user);
-//         } else {
-//             return Optional.empty();
-//         }
-//     }
+    public Optional<User> getUserById(String userId) throws Exception {
+        DocumentSnapshot snapshot = firestore.collection("Users").document(userId).get().get();
+        if (snapshot.exists()) {
+            User user = snapshot.toObject(User.class);
+            // System.out.println("Raw data: " + snapshot.getData());
+            // System.out.println("Fetched user: " + user);
+            return Optional.ofNullable(user);
+        } else {
+            return Optional.empty();
+        }
+    }
     
-// }
+}
