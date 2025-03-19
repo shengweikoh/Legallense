de043b83f57321b403b734fec2d2ea013aa887fe
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2025-03-19 14:44:38 +0800

    Update FirebaseConfig.java

--- LegaLensBackend/src/main/java/com/example/cs206/LegaLensBackend/config/FirebaseConfig.java
+++ LegaLensBackend/src/main/java/com/example/cs206/LegaLensBackend/config/FirebaseConfig.java
@@ -1,39 +1,39 @@
-// package com.example.cs206.LegaLensBackend.config;
+package com.example.cs206.LegaLensBackend.config;
 
-// import com.google.auth.oauth2.GoogleCredentials;
-// import com.google.firebase.FirebaseApp;
-// import com.google.firebase.FirebaseOptions;
-// import com.google.cloud.firestore.Firestore;
-// import com.google.firebase.cloud.FirestoreClient;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// import javax.annotation.PostConstruct;
-// import java.io.IOException;
-// import java.io.InputStream;
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
 
-// // Put serviceAccountKey.json in Resources folder.
+// Put serviceAccountKey.json in Resources folder.
 
-// @Configuration
-// public class FirebaseConfig {
+@Configuration
+public class FirebaseConfig {
 
-//     @PostConstruct
-//     public void initFirebase() throws IOException {
-//         InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+    @PostConstruct
+    public void initFirebase() throws IOException {
+        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
 
-//         if (serviceAccount == null) {
-//             throw new IOException("Firebase serviceAccountKey.json not found in resources.");
-//         }
+        if (serviceAccount == null) {
+            throw new IOException("Firebase serviceAccountKey.json not found in resources.");
+        }
 
-//         FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+        FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
 
-//         if (FirebaseApp.getApps().isEmpty()) {
-//             FirebaseApp.initializeApp(options);
-//         }
-//     }
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
 
-//     @Bean
-//     public Firestore fireStore() {
-//         return FirestoreClient.getFirestore();
-//     }
-// }
+    @Bean
+    public Firestore fireStore() {
+        return FirestoreClient.getFirestore();
+    }
+}
