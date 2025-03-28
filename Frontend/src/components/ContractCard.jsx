0307e3bd3f40f2205f683d1b8ab769c0bca322a1
weiqichua <143248754+weiqichua@users.noreply.github.com>
2025-03-28 14:57:18 +0800

    remove import

--- Frontend/src/components/ContractCard.jsx
+++ Frontend/src/components/ContractCard.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
-import "./ContractCard.css";
 
 /* ContractCard.jsx */
 export const ContractCard = ({
