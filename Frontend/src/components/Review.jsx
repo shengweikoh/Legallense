f575aff13e3f27cc9ad858d71b9e0fc4f3b77da9
weiqichua <143248754+weiqichua@users.noreply.github.com>
2025-03-29 17:52:08 +0800

    styling

--- Frontend/src/components/Review.jsx
+++ Frontend/src/components/Review.jsx
@@ -1,11 +1,6 @@
-import { ContractDataLoader } from './ContractDataLoader';
-import geminiApi from '../services/geminiApi';
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  Typography
-} from '@mui/material';
+import { ContractDataLoader } from "./ContractDataLoader";
+import geminiApi from "../services/geminiApi";
+import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 import "./markdown.css";
 
 export const Review = () => {
@@ -14,20 +9,18 @@ export const Review = () => {
       {(data) => (
         <div className="markdown" style={{ textAlign: "left" }}>
           {data.map((clause, index) => (
-            <Card
-              key={index}
-              variant="outlined"
-              sx={{ mb: 2}}
-            >
+            <Card key={index} variant="outlined" sx={{ mb: 2, borderRadius: 2, boxShadow: '0px 4px 10px rgba(0,0,0,0.15)',}}>
               <CardHeader
                 title={
                   <Typography variant="h5" component="div">
-                    <strong>Problem {index+1}: {clause.clauseName}</strong>
+                    <strong>
+                      Problem {index + 1}: {clause.clauseName}
+                    </strong>
                   </Typography>
                 }
               />
-          <CardContent sx={{ pb: 0, '&:last-child': { pb: 0 } }}> 
-            <Typography>
+              <CardContent sx={{ pb: 0, "&:last-child": { pb: 0 } }}>
+                <Typography>
                   <strong>Description:</strong> {clause.content1}
                 </Typography>
                 <Typography>
